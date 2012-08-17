447ee8eb0dd74e0a40a3d908ae963444e6b026f0
Mahmoud <mahmoud.khaled@badrit.com>
2012-08-17 15:34:28 +0200

    adding customers scaffold missing files

---
+++ db/migrate/20120817133128_create_customers.rb
@@ -0,0 +1,10 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :firstname
+      t.string :lastname
+
+      t.timestamps
+    end
+  end
+end
